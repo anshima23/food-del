20850b0f195e0484d7b328f29d8e53bc2dc26dd8
Anshima <anshima541@gmail.com>
2024-08-06 14:02:38 +0530

    sign up compl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home'; // Ensure this path is correct
 import Cart from './Pages/Cart/Cart'; // Ensure this component exists
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'; // Ensure this component exists
 import Footer from './Components/Footer/Footer';
+import LoginPopup from './Components/LoginPopup/LoginPopup'; // Adjust path as needed
+
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
