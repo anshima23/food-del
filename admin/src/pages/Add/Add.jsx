e369b794c6fe0df602f9670cf5b93221d95b93db
Anshima <anshima541@gmail.com>
2024-08-11 16:43:55 +0530

    admin work

---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import './Add.css'
+import { assets } from '../../assets/assets'
+import axios from "axios"
+
+const Add = () => {
+
+
+  const url ="http://localhost:4000";
+  const [image,setImage] = useState(false);
+  const [data,setData] = useState({
+    name:"",
+    description:"",
+    price:"",
+    category:"Salad"
+  })
+
+  const onChangeHandler = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setData(data=>({...data,[name]:value}))
+  }
+
+  const onSubmitHandler = async (event) =>{
+     FormData.append("name",data.name)
+     FormData.append("description",data.description)
+     FormData.append("price",Number(data.name))
+     FormData.append("category",data.category)
+     FormData.append("image",image)
+     const response = await axios.post(`${url}/api/food/add`,formData);
+     if(response.data.success){
+        
+     }
+     else{
+
+     }
+  }
+
+ 
+
+  return (
+    <div className='add'>
+      <form className='flex-col' onSubmit={onSubmitHandler}>
+        <div className='add-img-upload flex-col'>
+          <p>Upload Image</p>
+          <label htmlFor='image'>
+              <img src={image?URL.createObjectURL(image):assets.upload_area} alt=''/>
+          </label>
+          <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='image' hidden required/>
+        </div>
+
+        <div className="add-product-name flex-col">
+            <p>Product name</p>
+            <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here'/>
+        </div>
+
+        <div className="add-product-description flex-col">
+           <p>Product description</p>
+           <textarea onChange={onChangeHandler} value={data.description} name='description' rows="6" placeholder='Write content here'></textarea>
+        </div>
+
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+               <p>Product category</p>
+               <select onChange={onChangeHandler} name="category">
+                <option value="Salad">Salad</option>
+                <option value="Rolls">Rolls</option>
+                <option value="Deserts">Deserts</option>
+                <option value="Sandwich">Sandwich</option>
+                <option value="Cake">Cake</option>
+                <option value="Pure Veg">Pure Veg</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Noodles">Noodles</option>
+               </select>
+          </div>
+
+          <div className="add-price flex-col">
+        <p>Product price</p>
+        <input onChange={onChangeHandler} value={data.price} type='Number' name='price' placeholder='$20'/>
+          </div>
+        </div>
+         <button type='submit' className='add-btn'>ADD</button>
+      </form>
+    </div>
+  )
+}
+
+export default Add
